c3a6e6e5d94120de4c228f478e8ffff21b36c4fe
Chuck Lagumbay <chuck@im.skubbs.com>
2015-07-23 12:15:41 +0800

    x

--- applications/cms/controllers/Clinic.php
+++ applications/cms/controllers/Clinic.php
@@ -1,6 +1,6 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Clinic extends Base_Controller { 
+class Clinic extends Base_Controller {
 
 	public function __construct() {
 		parent::__construct();
