41674b821250541adfd2e8062f5bb06fde9665cc
Chuck Lagumbay <chuck@im.skubbs.com>
2015-08-14 12:04:23 +0800

    qwefdq

--- applications/cms/models/Model_medications.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class Model_Medications extends Base_Model {
-    public $data = array('view_file' => 'form_medications');
-    public function index($d) {
-        return $this->data;
-    }
-}
